09f9e3ef821c95dcb541701764a57cab2ba11277
Jörg Thalheim <joerg@thalheim.io>
2023-08-07 10:48:54 +0200

    add release script

---
+++ scripts/create-release.sh
@@ -0,0 +1,38 @@
+#!/usr/bin/env bash
+
+set -eu -o pipefail
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null && pwd )"
+cd "$SCRIPT_DIR/.."
+
+version=${1:-}
+if [[ -z "$version" ]]; then
+  echo "USAGE: $0 version" >&2
+  exit 1
+fi
+
+if [[ "$(git symbolic-ref --short HEAD)" != "master" ]]; then
+  echo "must be on master branch" >&2
+  exit 1
+fi
+
+# ensure we are up-to-date
+uncommited_changes=$(git diff --compact-summary)
+if [[ -n "$uncommited_changes" ]]; then
+  echo -e "There are uncommited changes, exiting:\n${uncommited_changes}" >&2
+  exit 1
+fi
+git pull git@github.com:nix-community/harmonia master
+unpushed_commits=$(git log --format=oneline origin/master..master)
+if [[ "$unpushed_commits" != "" ]]; then
+  echo -e "\nThere are unpushed changes, exiting:\n$unpushed_commits" >&2
+  exit 1
+fi
+sed -i -e "s!^version = \".*\"\$!version = \"${version}\"!" harmonia/Cargo.toml
+cargo build
+git add Cargo.lock harmonia/Cargo.toml
+nix flake check -vL
+git commit -m "bump version ${version}"
+git tag -e "${version}"
+
+echo "now run 'git push --tags origin master'"
