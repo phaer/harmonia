efb5202ac43aabd6376e7c8644640605fa8b2bb9
clerie <git@clerie.de>
2024-06-15 14:44:37 +0200

    Test signing with multiple keys

--- tests/t01-signing.nix
+++ tests/t01-signing.nix
@@ -4,13 +4,13 @@
     inherit (pkgs) hello;
     copyScript = pkgs.writeShellScript "copy-test" ''
       set -e
-      PUBKEY=$(cat ${./cache.pk})
+      PUBKEY=$(cat $1)
       nix copy \
         --option trusted-public-keys "$PUBKEY" \
         --from http://harmonia:5000 \
         --extra-experimental-features nix-command \
-        --to /root/test-store \
-        "$@"
+        --to "$2" \
+        "$3"
     '';
   in
   {
@@ -22,7 +22,7 @@
 
         services.harmonia-dev = {
           enable = true;
-          signKeyPath = "${./cache.sk}";
+          signKeyPaths = [ "${./cache.sk}" "${./cache2.sk}" ];
         };
 
         networking.firewall.allowedTCPPorts = [ 5000 ];
@@ -40,7 +40,8 @@
       client01.wait_until_succeeds("curl -f http://harmonia:5000/version")
       client01.succeed("curl -f http://harmonia:5000/nix-cache-info")
 
-      client01.wait_until_succeeds("${copyScript} ${hello}")
+      client01.wait_until_succeeds("${copyScript} ${./cache.pk} /root/test-store ${hello}")
+      client01.wait_until_succeeds("${copyScript} ${./cache2.pk} /root/test-store2 ${hello}")
       client01.succeed("${hello}/bin/hello --version")
     '';
   })
